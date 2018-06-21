9c789148ee023763751d033a30996851287a015c
Diogo Batista <diogobatista@ua.pt>
2018-06-21 16:37:15 +0000

    better debugging tools

--- SoundGen.py
+++ SoundGen.py
@@ -21,15 +21,15 @@ global frame_size
 
 # configure SDR device
 sdr.sample_rate = 226e3
-sdr.center_freq = 90010031
-sdr.gain = 20
+sdr.center_freq = 90010100
+sdr.gain = 14
 frame_size = 16 * 1024 # 32
 
 
 #signal characteristics
 
 decimation_factor = 1 									# It might be possible to increase efficiency by decimating the signal before it gets passed along to the pdata library, paceholder for now
-signal_frequency = 3650 * decimation_factor				# Baseband frequency of the desired signal it should be no higher than half of the SDR kit sampling rate
+signal_frequency = 3650 * decimation_factor				# Baseband frequency of the desired signal it should be no higher than one tenth of the SDR kit sampling rate
 bits_per_word = 32										# How many bits of information will be arriving in burst in each recieved message
 
 signal_period = 1/signal_frequency						
@@ -52,8 +52,8 @@ sample_buffer = queue.Queue(buffer_size)
 
 global iteration_counter, stop_at
 iteration_counter = 0									# Counts the number of frames collected so far
-stop_at = 25											# How many frames of data the program will collect and process before it ends
-debug = False											# Debug capabilities switch
+stop_at = 10											# How many frames of data the program will collect and process before it ends
+debug = True											# Debug capabilities switch
 
 ## Debugging variables
 
@@ -73,11 +73,9 @@ def collectData(): 	#Collect samples
 	global frame_size
 	global iteration_counter, flag_end, stop_at
 	
-	
 	t = time.time()
 	while iteration_counter < stop_at:
 		sample_buffer.put_nowait(abs(sdr.read_samples(frame_size)))  ## Harvests samples and stores their ABSOLUTE VALUES into a FIFO
-		#sample_buffer.put_nowait(scipy.signal.decimate(abs(sdr.read_samples(frame_size))), 10)  ## Harvests samples and stores their ABSOLUTE VALUES into a FIFO
 		iteration_counter += 1
 	print("\n###TERMINEI A RECOLHA DE AMOSTRAS EM " + str(round(time.time() -t, 3)) + ". TEMPO IDEAL = " +str(round((frame_size*stop_at)/sdr.sample_rate, 3)) + "###\n")
 	flag_end = True
@@ -125,9 +123,9 @@ if __name__ == "__main__":
 			  
 			this_frame = sample_buffer.get_nowait()								
 			
-			demod_signal = pdata.process_data(this_frame, samples_per_bit, frame_size)
+			demod_signal = pdata.process_data(this_frame, samples_per_bit, frame_size) 	# O sinal cru é desmodulado (ASK) através das funções da biblioteca pdata
 			#print(demod_signal)
-			end_result.extend(demod_signal)
+			end_result.extend(demod_signal)												# O resultado obtido da desmodulação é anexado ao fim do array end_result
 			
 			
 			if debug == True:
@@ -139,15 +137,16 @@ if __name__ == "__main__":
 		if end_result[x:x+len(desired_result)] == desired_result:
 			sucesses += 1
 			
-
+	print(end_result)
 	
 	t_collector.join()
 	#time.sleep(1)
 	print("\nFINISHED   \n\nActive threads: " + str(threading.activeCount()) + "\nIterations: " +  str(iteration_counter) + "\nSamples processed: " + str(frame_size*stop_at) + "\nSucesses: " + str(sucesses) + "\nRuntime: "  +  str(round(time.time() -t, 3)) )	
 	
 	if debug == True:
-		save('outfile', allsamples) 
-	
+		save('outfile_samples', allsamples) 
+		save('outfile_signal', end_result)
+		save('outfile_SPB', samples_per_bit)
 		
 	sys.exit(main(sys.argv))
 
--- debug.py
+++ debug.py
@@ -6,12 +6,30 @@ def main(args):
 
 if __name__ == '__main__':
     
-	allsamples = pylab.load('outfile.npy')
+	allsamples = pylab.load('outfile_samples.npy')
+	end_result = pylab.load('outfile_signal.npy')
+	samples_per_bit = pylab.load('outfile_SPB.npy')
 	print("Sample Size: " + str(len(allsamples)))
 	
+	demod_signal=[0.5 for x in allsamples]
+	counter = 1
+	
+	
+	for x in range(len(end_result)):
+		a = int(round(samples_per_bit * (counter-1)))
+		b = int(round(samples_per_bit * counter))
+		for c in range(a,b):
+			demod_signal[c] = end_result[x]
+		counter += 1
+			
 	pylab.ioff()
 	x = pylab.arange(len(allsamples))
+	pylab.subplot(211)
 	pylab.plot(x, allsamples)
+	pylab.subplot(212)
+	pylab.plot(x, demod_signal, 'r')
+	
+	
 	pylab.show()
         
 	sys.exit(main(sys.argv))
