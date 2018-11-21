18ebc9bc57209ce627abb0b71762287552c1bd6f
Unknown <diogo_speedragon_batista@hotmail.com>
2018-11-21 15:19:19 +0000

    Added real time filtering to filter simulator

--- filterSim.py
+++ filterSim.py
@@ -1,21 +1,20 @@
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
+import filterGen
 
 
 #samples = np.load("outfile_samples.npy")
 samples2 = np.load("PBZS_stage1.npy")
 pre_filter = np.load('pre_filter.npy')
-post_filter = np.load('post_filter.npy')
 
-window = 65 * 40
 
-start = 167000
-start2 = 452500
 
-pre_filter = pre_filter[start:start+window]
-post_filter = post_filter[start2:start2+window]
 
+window = 65 * 120
+
+start = 201000
+start2 = 201000
 
 sample_rate = 226000
 symbol_rate = 3650
@@ -24,8 +23,13 @@ nyq_freq = sample_rate/2
 
 filter_order = 2
 low_cutoff = 15
-high_cutoff = 3600
+high_cutoff = 3650
 
+post_filter = filterGen.bp_butter(pre_filter, [low_cutoff, high_cutoff], filter_order, sample_rate)
+print('post filter lenght: ' + str(len(post_filter)))
+
+pre_filter = pre_filter[start:start+window]
+post_filter = post_filter[start2:start2+window]
 
 w1 = low_cutoff / nyq_freq          # For digital filters, Wn is normalized from 0 to 1, where 1 is the Nyquist frequency, pi radians/sample.
 w2 = high_cutoff / nyq_freq         # (Wn is thus in half-cycles / sample.)
@@ -44,7 +48,9 @@ plt.title('Bandpass over Power Spectral Density')
 plt.axvline(low_cutoff, color='green') # cutoff frequency
 plt.axvline(high_cutoff, color='green') # cutoff frequency
 plt.xlim([0, high_cutoff*ratio])
-plt.psd(samples2, NFFT=4096, Fs=sample_rate, Fc=0)
+pre_psd = plt.psd(pre_filter, NFFT=2048, Fs=sample_rate, Fc=0)
+post_psd = plt.psd(post_filter, NFFT=2048, Fs=sample_rate, Fc=0)
+plt.legend(['FcL','FcH','Pre-Filter', 'Post-Filter'])
 
 
 
